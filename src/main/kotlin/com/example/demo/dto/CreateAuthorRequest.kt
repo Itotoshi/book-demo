f061ae690d77c471d7051170b4e5bd1d48610013
Yosian <nananana7sian@yahoo.co.jp>
2025-09-05 18:40:11 +0900

    CreateAuthorRequest.kt 書籍作成用DTOの新規作成

---
+++ src/main/kotlin/com/example/demo/dto/CreateAuthorRequest.kt
@@ -0,0 +1,13 @@
+package com.example.demo.dto
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Past
+import java.time.LocalDate
+
+data class CreateAuthorRequest (
+    @field:NotBlank
+    val name: String,
+
+    @field:Past
+    val birthDay: LocalDate
+)
