ba902b348e203a301dfd40494f1daeec28a2971b
Yosian <nananana7sian@yahoo.co.jp>
2025-09-11 14:56:29 +0900

    Books apiパスを更新

--- src/main/kotlin/com/example/demo/controller/BookController.kt
+++ src/main/kotlin/com/example/demo/controller/BookController.kt
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/api/books")
 class BookController(private val bookService: BookService) {
     /**
      * 書籍を新規登録
